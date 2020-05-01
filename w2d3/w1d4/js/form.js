07bbe2b26f5311893ab2e7fe41fc3742133da61d
ankundaian <60825097+ankundaian@users.noreply.github.com>
2020-05-01 09:14:08 -0500

    w2d3 added

--- w2d3/w1d4/js/form.js
+++ w2d3/w1d4/js/form.js
@@ -9,25 +9,6 @@ $(document).ready(function () {
         console.log($("#website").val());
     });
 
-//     $("productForm").on("submit",
-//         function(evt){
-//             evt.preventDefault();
-//             console.log($("#productNumber").val());
-//             // $("#displayDiv").append(
-//             //     $("<p>", {  "text": "My paragraph is awesome!" });
-//             // );
-//             // $("#displayDiv").text(
-//             //     `${$("#productNumber").val()}`
-//             // );
-//             $("#displayDiv").html(
-//                 "Product Number : " + $("#productNumber").val() + "</br>" +
-//                 "Product Name : " + $("#name").val() + "</br>" +
-//                 "Product Number : " + $("#unitPrice").val() + "</br>" +
-//                 "Quantity : " + $("#quantity").val() + "</br>" +
-//                 "Supplier : " + $("#supplier").val() + "</br>" +
-//                 "Date Supplied : " + $("#date-supplied").val() + "</br>" 
-//             );
-//         });
 
 });
 
--- w2d3/w1d4/js/form2.js
+++ w2d3/w1d4/js/form2.js
@@ -4,14 +4,22 @@ $(document).ready(function () {
         $("#productForm").on("submit",
         function(evt){
             evt.preventDefault();
-            $("#displayDiv").html(
-                "Product Number : " + $("#productNumber").val() + "</br>" +
-                "Product Name : " + $("#name").val() + "</br>" +
-                "Product Number : " + $("#unitPrice").val() + "</br>" +
-                "Quantity : " + $("#quantity").val() + "</br>" +
-                "Supplier : " + $("#supplier").val() + "</br>" +
-                "Date Supplied : " + $("#date-supplied").val() + "</br>" 
-            );
+            // $("#displayDiv").html(
+            //     "Product Number : " + $("#productNumber").val() + "</br>" +
+            //     "Product Name : " + $("#name").val() + "</br>" +
+            //     "Uuit Price : " + $("#unitPrice").val() + "</br>" +
+            //     "Quantity : " + $("#quantity").val() + "</br>" +
+            //     "Supplier : " + $("#supplier").val() + "</br>" +
+            //     "Date Supplied : " + $("#date-supplied").val() + "</br>" 
+            // );
+            $("#displayDiv").append(
+                `<p>Product Number :  ${$("#productNumber").val()} </p>
+                <p>Product Name :  ${$("#name").val()} </p>
+                <p>Unit :  ${$("#unitPrice").val()} </p>
+                <p>Quantity :  ${$("#quantity").val()} </p>
+                <p>Supplier :  ${$("#supplier").val()} </p>
+                <p>Date Supplied :  ${$("#date-supplied").val()} </p>`
+            )
         });
 });
 
